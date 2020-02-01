85fcb9476e002f2e7236ac8831be5b86f75fcf5c
YONGJAE NAM <34469792+YJNN@users.noreply.github.com>
2020-02-01 20:12:46 +0900

    Update waitingBeaconActivity.java

--- SureWhyNot2/app/src/main/java/com/example/yongjaenam/surewhynot/waitingBeaconActivity.java
+++ SureWhyNot2/app/src/main/java/com/example/yongjaenam/surewhynot/waitingBeaconActivity.java
@@ -12,13 +12,9 @@ import android.view.MotionEvent;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
 import com.estimote.sdk.SystemRequirementsChecker;*/
-
-
 /**
  * Created by Yongjae Nam on 2017-11-08.
  */
-
-//처음 대기화면에서 이 화면으로 넘어오면, 비콘을 기다리는 액티비티.
 public class waitingBeaconActivity extends AppCompatActivity{
     //private long mShakeTime;
     //private static final int SHAKE_SKIP_TIME = 500;
@@ -33,30 +29,8 @@ public class waitingBeaconActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.waitingbeacon);
-        /*
-        beaconManager = new BeaconManager(this);
-        beconManager.setRangingListener(new BeaconManager.RangingListener() {
-            @Override
-            public void onBeaconDiscovered(Region region, List<Beacon> list) {
-                if (!list.isEmpty()) {
-                    Beacon nearestBeacon = list.get(0);
-                    Log.d("dfdf", "dfdf: " + nearestBeacon.getRssi()); //nearestBeacon.getRssi()는 비콘의 수신강도를 나타냄.
-
-                    if (!isConnected && nearestBeacon.getRssi() > 20) {
-                        isConnected = true;
-                        //startActivity(new Intent(this, Choice.class));   //비콘들어오면 화면넘기기
-                    } else if (isConnected && nearestBeacon.getRssi() < 20) {
-                        isConnected = false;
-                    }
-
-                }
-            }
-        });
-        region= new Region("ranged region", UUID.fromString("77777777777"), 0, 0);*/
     }
-
-
-
+    
     /*@Override
     protected void onResume(){
         super.onResume();
@@ -75,10 +49,8 @@ public class waitingBeaconActivity extends AppCompatActivity{
     }*/
 
     private class NaverTTSTask extends AsyncTask<String[], Void, String> {
-
         @Override
         protected String doInBackground(String[]... strings) {
-            //여기서 서버에 요청
             APIExamTTS.main(mTextString, getFilesDir());
             return null;
         }
@@ -91,7 +63,7 @@ public class waitingBeaconActivity extends AppCompatActivity{
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if(event.getAction() == MotionEvent.ACTION_DOWN){ //눌렀을 때
+        if(event.getAction() == MotionEvent.ACTION_DOWN){ 
             startActivity(new Intent(this, Choice.class));
             finish();
         }
